cc08512d48b4be27f543aea659b49c14a0f15c17
icejoywoo <icejoywoo@gmail.com>
2014-04-29 22:20:29 +0800

    add enum class singleton

---
+++ src/main/java/singleton/enumsingleton/Singleton.java
@@ -0,0 +1,14 @@
+package singleton.enumsingleton;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: icejoywoo
+ * Date: 14-4-29
+ * Time: 下午10:19
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Singleton {
+    INSTANCE;
+
+    public void leaveTheBuilding() {}
+}
